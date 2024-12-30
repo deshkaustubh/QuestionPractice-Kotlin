e7d68e35d9ac79407749c97a1b3f3ef7a44c8f37
Kaustubh <ksd.tcet@gmail.com>
2024-12-30 10:50:18 +0530

    initial push

---
+++ src/main/kotlin/stringQuestions/StringToTIleCase.kt
@@ -0,0 +1,20 @@
+package org.example.stringQuestions
+
+/*
+Convert String to Title Case
+Given a string S consisting of only lowercase and uppercase English letters and spaces, your task is to convert it into title case. In title case, the first letter of each word is capitalized while the rest are in lowercase, except for words that are entirely in uppercase (considered as acronyms), which should remain unchanged.
+
+Note:
+
+Words are defined as contiguous sequences of English letters separated by spaces.
+Acronyms are words that are entirely in uppercase and should remain unchanged.
+Assume the input does not contain leading, trailing, or multiple spaces between words.
+Input Format
+The first line contains a single integer T, the number of test cases.
+Each of the next T lines contains a string S.
+Output Format
+For each test case, print a single line containing the string S converted into title case.
+ */
+
+
+// Can't Figure out
