cb5887c7944565f9d4fd872ab36f69da418d542c
Kaustubh <ksd.tcet@gmail.com>
2025-01-21 09:22:58 +0530

    #3 Necessary Modifications

--- src/main/kotlin/stringQuestions/ReversedListInPlace.kt
+++ src/main/kotlin/stringQuestions/ReversedListInPlace.kt
@@ -4,19 +4,24 @@ package org.example.stringQuestions
 // Mutable List ❌❌❌
 // Adds from last and removes from front
 
-//fun main() {
-//    val list = listOf(1, 2, 3, 4, 5, 6)
-//    println(reversedList(list))
-//}
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5, 6, 7)
+    println(reversedList(list))
+}
 
-//fun reversedList(list: List<Int>): List<Int> {
-//
-//    var countBackward = list.size
-//    var countForward = 0
-//    repeat(list.size) {
-//        val temp = list[it]
-//        list[countForward] = temp
-//    }
-//
-//    return list
-//}
+fun reversedList(list: List<Int>): List<Int> {
+    val mutableList = list.toMutableList()
+    var forwardCount = 0
+    var backwardCount = list.size - 1
+
+    repeat(list.size / 2) {
+
+        val temp = mutableList[forwardCount]
+        mutableList[forwardCount] = mutableList[backwardCount]
+        mutableList[backwardCount] = temp
+
+        forwardCount++
+        backwardCount--
+    }
+    return mutableList
+}
