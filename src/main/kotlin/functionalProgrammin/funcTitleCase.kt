7ab251c9e62947a0db87b196fd71039fe5796d96
Kaustubh <ksd.tcet@gmail.com>
2025-01-23 10:41:24 +0530

    split functional programming

--- src/main/kotlin/functionalProgrammin/funcTitleCase.kt
+++ src/main/kotlin/functionalProgrammin/funcTitleCase.kt
@@ -1,2 +1,34 @@
 package org.example.functionalProgrammin
 
+
+fun titleCase(s:String): String  {
+    return s.split(" ").joinToString(" ") { word->
+        if (word.length == 1) {
+            word.uppercase()
+        } else if(word == word.uppercase()) {
+            word
+        } else {
+            word[0].uppercaseChar() + word.substring(1).lowercase()
+        }
+    }
+}
+
+
+
+fun main() {
+    val t = readln().toInt()
+    repeat(t) {
+        val s = readln()
+        println(titleCase(s))
+    }
+}
+
+//fun convertToTitleCase(s: String): String {
+//    return s.split(" ").joinToString(" ") { word ->
+//        if (word.all { it.isUpperCase() }) {
+//            word // Keep acronyms unchanged
+//        } else {
+//            word.toLowerCase().capitalize() // Convert to title case
+//        }
+//    }
+//}
