305c40ef8e6775ee53d1fd5ebb5ee0563082cf94
Kaustubh <ksd.tcet@gmail.com>
2025-01-26 18:55:03 +0530

    iterative approach added

--- src/main/kotlin/functionalProgrammin/task1Chunked.kt
+++ src/main/kotlin/functionalProgrammin/task1Chunked.kt
@@ -3,9 +3,29 @@ package org.example.functionalProgrammin
 
 fun main() {
     val givenString = "00101110"
-    println(chunkedString(givenString))
+    println(iterativeChunked(givenString))
 }
 
 fun chunkedString(string: String): List<String> {
     return string.chunked(2)
 }
+
+// Output: [00, 10, 11, 10]
+
+fun iterativeChunked(string: String): List<String> {
+    val outputList = mutableListOf<String>()
+    var tempString = ""
+    for (num in string.indices) {
+        tempString += string[num]
+        if(tempString.length == 2) {
+          outputList.add(tempString)
+            tempString = ""
+        }
+    }
+    if (tempString.isNotEmpty()) {
+        outputList.add(tempString)
+    }
+    return outputList
+}
+
+
