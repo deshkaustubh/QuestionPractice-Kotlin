ef8c8d32545d67125ead9dd5d19f8ba5ed6deef4
Kaustubh <ksd.tcet@gmail.com>
2025-01-26 20:05:55 +0530

    iterative approach added

--- src/main/kotlin/functionalProgrammin/task4Windowed.kt
+++ src/main/kotlin/functionalProgrammin/task4Windowed.kt
@@ -2,9 +2,33 @@ package org.example.functionalProgrammin
 
 fun main() {
     val givenString = "00101110"
-    println(windowed(givenString, 2))
+    println(iterativeWindowed(givenString, 3, ))
+    println(windowed(givenString, 3, ))
 }
 
 fun windowed(string: String, size: Int, step: Int = 1): List<String> {
     return string.windowed(size, step = step)
+}
+
+//OUTPUT -> [00, 01, 10, 01, 11, 11, 10]
+
+fun iterativeWindowed(string: String, size: Int, step: Int = 1): List<String> {
+    val outputList = mutableListOf<String>()
+
+
+    var counter = 0
+
+    while( counter != string.length - step) {
+        var tempString = ""
+        var tempInt = counter
+        repeat(size) {
+            if(tempInt < string.length) {
+                tempString += string[tempInt]
+                tempInt++
+            }
+        }
+        outputList.add(tempString)
+        counter += step
+    }
+    return outputList
 }
