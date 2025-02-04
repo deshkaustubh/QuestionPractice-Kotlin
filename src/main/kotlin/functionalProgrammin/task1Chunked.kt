1e06b7c375907e80901fe6fa02f1eb85fbd73fc1
Kaustubh <ksd.tcet@gmail.com>
2025-02-04 10:54:30 +0530

    use of buildList and buildString

--- src/main/kotlin/functionalProgrammin/task1Chunked.kt
+++ src/main/kotlin/functionalProgrammin/task1Chunked.kt
@@ -12,20 +12,35 @@ fun chunkedString(string: String): List<String> {
 
 // Output: [00, 10, 11, 10]
 
+//fun iterativeChunked(string: String): List<String> {
+//    val outputList = mutableListOf<String>()
+//    var tempString = ""
+//    for (num in string.indices) {
+//        tempString += string[num]
+//        if(tempString.length == 2) {
+//          outputList.add(tempString)
+//            tempString = ""
+//        }
+//    }
+//    if (tempString.isNotEmpty()) {
+//        outputList.add(tempString)
+//    }
+//    return outputList
+//}
+
 fun iterativeChunked(string: String): List<String> {
-    val outputList = mutableListOf<String>()
-    var tempString = ""
-    for (num in string.indices) {
-        tempString += string[num]
-        if(tempString.length == 2) {
-          outputList.add(tempString)
-            tempString = ""
+    return buildList<String> {
+        buildString {
+            for (i in string) {
+                append(i)
+                if (length == 2) {
+                    add(toString()) // converts the current content in string builder to a string
+                    setLength(0) // sets string builder length to 0
+                }
+            }
+            // for char string length less than 2
+            if (isNotEmpty()) add(toString())
         }
     }
-    if (tempString.isNotEmpty()) {
-        outputList.add(tempString)
-    }
-    return outputList
 }
-
 
