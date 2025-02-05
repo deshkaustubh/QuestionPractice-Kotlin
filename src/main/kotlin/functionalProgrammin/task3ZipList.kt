2058c37e46a7ed0fc70938933ea0c02ad657c889
Kaustubh <ksd.tcet@gmail.com>
2025-02-05 18:03:50 +0530

    #5 changed max to min

--- src/main/kotlin/functionalProgrammin/task3ZipList.kt
+++ src/main/kotlin/functionalProgrammin/task3ZipList.kt
@@ -13,7 +13,7 @@ fun zipLists(l1: List<Int>, l2: List<Int>): List<Pair<Int, Int>> {
 // OUTPUT -> [(1, 6), (2, 7), (3, 8), (4, 9)]
 
 fun iterativeZipLists(l1: List<Int>, l2: List<Int>): List<Pair<Int, Int>> {
-    val length = if (l1.size > l2.size) l1.size else l2.size // maxOf(l1.size, l2.size)
+    val length = if (l1.size < l2.size) l1.size else l2.size // minOf(l1.size, l2.size)
     val outputList = mutableListOf< Pair<Int, Int>>()
 
     repeat(length) { // since we have calculated length from string there isn't need of subtracting 1
