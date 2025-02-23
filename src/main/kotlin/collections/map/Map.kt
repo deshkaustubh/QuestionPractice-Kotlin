3310673d3863014423de0f6c06c74262a2549ddd
Kaustubh <ksd.tcet@gmail.com>
2025-02-23 17:34:19 +0530

    Initial Commit

---
+++ src/main/kotlin/collections/map/Map.kt
@@ -0,0 +1,29 @@
+package collections.map
+
+// Map data structure is a list of pair pair<K,V> where K and V can be any data  type
+
+// each pair is = entry
+// first value -> key || second value -> value
+
+fun main() {
+    // Create the map
+    val dobsMap = mapOf(
+        "A" to "1/1/2001",
+        "B" to "1/2/2002",
+        "C" to "1/1/2003"
+    )
+
+    // Input name
+    print("Enter name of person : ")
+    val name = readln()
+
+    // Retrieve DOB
+    val dob = dobsMap[name]
+
+    // Handle found / not found case
+    if (dob != null) {
+        println("DOB of $name = $dob")
+    } else {
+        println("DOB of $name not found!")
+    }
+}
