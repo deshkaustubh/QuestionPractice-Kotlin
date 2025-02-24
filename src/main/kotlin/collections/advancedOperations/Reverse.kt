283e1a618f989bdb94769b59855f10c8752e661b
Kaustubh <ksd.tcet@gmail.com>
2025-02-25 00:46:58 +0530

    Initial Commit

---
+++ src/main/kotlin/collections/advancedOperations/Reverse.kt
@@ -0,0 +1,12 @@
+package collections.advancedOperations
+
+/*
+To reverse a collection , we can use the reversed() function. It creates and returns a new list in reversed order:
+SYNTAX
+    coll.reversed()
+ */
+
+fun main() {
+    val nums = listOf(1,2,3)
+    println(nums.reversed())
+}
