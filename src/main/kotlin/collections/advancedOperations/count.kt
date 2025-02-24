f0d0046beb325d35b3c63cf4d08ce2af8fcd28bc
Kaustubh <ksd.tcet@gmail.com>
2025-02-24 23:02:23 +0530

    Initial Commit

---
+++ src/main/kotlin/collections/advancedOperations/count.kt
@@ -0,0 +1,19 @@
+package collections.advancedOperations
+
+
+// Used to count the number of elements in a collection that match a given predicate / condition
+
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+    // Count the number of even numbers in the list
+    val evenCount = numbers.count { it % 2 == 0 }
+
+    println("The number of even numbers in the list: $evenCount")
+}
+/*
+In this example,
+the count function iterates through the list numbers and counts how many elements satisfy the condition it % 2 == 0
+(i.e., the number is even). The result is then printed out,
+showing the count of even numbers in the list.
+ */
