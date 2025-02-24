7bc855ac70c3054d892e4e7f177c14914b7abdd9
Kaustubh <ksd.tcet@gmail.com>
2025-02-24 22:34:10 +0530

    updates

--- src/main/kotlin/collections/advancedOperations/AllandAny.kt
+++ src/main/kotlin/collections/advancedOperations/AllandAny.kt
@@ -4,3 +4,11 @@ package collections.advancedOperations
 all -> returns true only if condition holds true for all elements ===> coll.all{.....}
 any -> returns true if predicate holds true for at least one element ===> col.any{.....}
 */
+
+fun main() {
+    val nums = listOf(-1, 0, 1)
+    println("all > 0 ? ${nums.all { it > 0 }}") //OUTPUT = false
+    println("at least one > 0 ? ${nums.any { it > 0 }}") // OUTPUT = true
+}
+
+// Special case: When collections is empty, all() returns true and any() returns false
