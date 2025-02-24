d712a8d0ab7345f975a020aa36ff6b0beb8ce9d9
Kaustubh <ksd.tcet@gmail.com>
2025-02-24 15:33:43 +0530

    Initial Commit

---
+++ src/main/kotlin/collections/advancedOperations/AllandAny.kt
@@ -0,0 +1,6 @@
+package collections.advancedOperations
+
+/* used to check a condition,
+all -> returns true only if condition holds true for all elements ===> coll.all{.....}
+any -> returns true if predicate holds true for at least one element ===> col.any{.....}
+*/
