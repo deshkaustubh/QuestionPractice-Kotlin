ad220a9e11e21a4e819ef28ea0bd36c20323f464
Kaustubh <ksd.tcet@gmail.com>
2025-02-27 19:06:50 +0530

    Initial Commit

---
+++ src/main/kotlin/functionalProgrammingConcepts/functional.kt
@@ -0,0 +1,12 @@
+package functionalProgrammingConcepts
+
+/*
+Functional Programming enables Declarative programming as opposed to Imperative programming. In Declarative programming, we write what needs to be done. Whereas in Imperative programming, we write how to do it.
+Composition of function refers to calling multiple functions in an ordered way such that output of one becomes input of next. We get output by applying multiple functions in order, on the input.
+Functions are first-class citizens i.e. functions can be saved as variables, passed as function arguments & can also be returned from a function.
+All three features might seem like alien concepts. We’ll understand each of these concepts with appropriate examples in later sections.
+
+Kotlin fully supports Functional programming. In Kotlin, functions are first-class citizens i.e. functions can be saved as variables, passed as function arguments & can also be returned from a function. For simplicity, we will call such functions as Lambda functions.
+
+To enable Functional Programming, Kotlin provides features like Function types, Lambda expressions and Higher-order functions. Let us try to understand each of these features
+ */
